b1845d59a4075dbf25608856614f1faabb2eae36
theba <theba.limbu07@gmail.com>
2023-08-22 08:18:47 +0100

    second commit

--- my_functions.py
+++ my_functions.py
@@ -3,6 +3,15 @@ import os
 MY_FILES = ['index.html', 'style.css', 'app.js']
 
 
+html_args = [
+            '<!DOCTYPE html>\n',
+            '<html>\n<head>\n <link rel="stylesheet" href="style.css">\n </head>\n',
+            '<body>\n <script src="app.js"></script>\n </body>\n</html>'
+             ]
+
+HTML_BOILERPLATE = f'{html_args[0]} {html_args[1]} {html_args[2]}'
+
+
 # function to create files
 def create_files(files):
     for file in files:
